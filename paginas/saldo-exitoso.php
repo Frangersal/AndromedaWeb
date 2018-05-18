7b614909005279b9d7248a8d266d2eb81dd4c54c
AlinaSM <alinasalmen995@gmail.com>
2018-05-18 01:46:45 -0500

    pagina saldo exitoso

---
+++ paginas/saldo-exitoso.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="es">
+
+    <head>
+        <title>Andromeda Store</title>
+        <link rel="stylesheet" href="../css/estilos.css">
+    </head>
+
+    <body>    
+    
+        
+    <div class="Banner"> 
+            <table>
+                <tr>
+                    <td><img src="../imagenes/logo.png"></td>
+                    <td><a href="../index.php">Entrar</a></td>
+                    <td><a href="registrarse.php">Registrarse</a></td>
+                    <td><a href="carrito.php">Carrito</a></td>
+                </tr>
+            </table>
+        </div>
+
+        
+        <div id="header">
+            <ul class="nav">
+                <li><a href="sesion-iniciada.php">Inicio</a></li>
+                <li><a href="">Productos</a>
+                    <ul>
+                        <li><a href="laptops.php">Laptops</a></li>
+                        <li><a href="pc.php">PC</a></li>
+                    </ul>
+                </li>
+                <li><a href="perfil.php">Perfil</a></li>
+            </ul>
+        </div>
+<br>
+    <div class="Letreros">
+            Saldo agregado
+        </div>
+
+        <div class="Estante">
+           <table>
+                <tr>
+                    
+                    <td>
+                        Saldo agregado de forma exitosa, puede comprar productos libremente.
+                    </td>
+                    
+                </tr>
+                <tr>
+                   
+                    <td>
+                        <img src="../imagenes/logo.png">
+                    </td>
+                   
+                </tr>
+
+            </table>
+        </div> 
+
+        
+    </body>
+</html>
